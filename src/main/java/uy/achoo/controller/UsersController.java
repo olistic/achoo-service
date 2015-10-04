5cda13539a13ddcd304178e77423f86498d31ccb
alfredoelters <aelters@lithium.com.uy>
2015-10-04 15:01:19 -0200

    Add preliminary authentication without jwt

--- src/main/java/uy/achoo/controller/UsersController.java
+++ src/main/java/uy/achoo/controller/UsersController.java
@@ -9,10 +9,7 @@ import uy.achoo.model.tables.pojos.User;
 import uy.achoo.util.DigestUtils;
 
 import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
@@ -23,35 +20,51 @@ import java.util.List;
 public class UsersController {
 
     public static User createUser(User user) throws NoSuchAlgorithmException, UnsupportedEncodingException, SQLException {
-        // Open the database connection
         Connection connection = DBConnector.getInstance().connection();
+        try {
+            // Initialize a Configuration
+            Configuration configuration = new DefaultConfiguration().set(connection).set(SQLDialect.MYSQL);
 
-        // Initialize a Configuration
-        Configuration configuration = new DefaultConfiguration().set(connection).set(SQLDialect.MYSQL);
+            // Initialize the DAO with the Configuration
+            UserDao userDao = new UserDao(configuration);
+            byte[] salt = DigestUtils.getSalt();
 
-        // Initialize the DAO with the Configuration
-        UserDao userDao = new UserDao(configuration);
-        byte[] salt = DigestUtils.getSalt();
-
-        user.setPassword(DigestUtils.digestPassword(user.getPassword(), salt));
-        user.setSalt(DigestUtils.byteToBase64(salt));
-        // Insert the POJO
-        userDao.insert(user);
-
-        // Close the database connection
-        connection.close();
-        return user;
-
+            user.setPassword(DigestUtils.digestPassword(user.getPassword(), salt));
+            user.setSalt(DigestUtils.byteToBase64(salt));
+            // Insert the POJO
+            userDao.insert(user);
+            return user;
+        } finally {
+            connection.close();
+        }
     }
 
     public static List<User> findAllUsers() throws SQLException {
         Connection connection = DBConnector.getInstance().connection();
+        try {
+            // Initialize a Configuration
+            Configuration configuration = new DefaultConfiguration().set(connection).set(SQLDialect.MYSQL);
+            List<User> users = new UserDao(configuration).findAll();
+            return users;
+        } finally {
+            connection.close();
+        }
+    }
 
-        // Initialize a Configuration
-        Configuration configuration = new DefaultConfiguration().set(connection).set(SQLDialect.MYSQL);
-        List<User> users = new UserDao(configuration).findAll();
-        connection.close();
-        return users;
-
+    public static boolean checkUsersPassword(String email, String password) throws UnsupportedEncodingException, NoSuchAlgorithmException, SQLException {
+        Connection connection = DBConnector.getInstance().connection();
+        try {
+            boolean result = false;
+            // Initialize a Configuration
+            Configuration configuration = new DefaultConfiguration().set(connection).set(SQLDialect.MYSQL);
+            User user = new UserDao(configuration).fetchOneByEmail(email);
+            if (user != null) {
+                String hashedPassword = DigestUtils.digestPassword(password, DigestUtils.base64toBytes(user.getSalt()));
+                result = hashedPassword.equals(user.getPassword());
+            }
+            return result;
+        } finally {
+            connection.close();
+        }
     }
 }
--- src/main/java/uy/achoo/rest/UsersResource.java
+++ src/main/java/uy/achoo/rest/UsersResource.java
@@ -9,8 +9,11 @@ import uy.achoo.model.tables.daos.UserDao;
 import uy.achoo.model.tables.pojos.User;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
@@ -34,7 +37,7 @@ public class UsersResource {
 
     @GET
     public Response index() {
-        Response response = null;
+        Response response;
         try {
             List<User> users = UsersController.findAllUsers();
             response = Response.status(200).entity(users).build();
@@ -48,11 +51,26 @@ public class UsersResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response create(User user) {
-        Response response = null;
+        Response response;
         try {
             User createdUser = UsersController.createUser(user);
             response = Response.status(200).entity(createdUser).build();
         } catch (NoSuchAlgorithmException|UnsupportedEncodingException |SQLException  e ) {
+            e.printStackTrace();
+            response = Response.status(500).entity(e).build();
+        }
+        return response;
+    }
+
+    @POST
+    @Path("authenticate")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response authenticate(@FormParam("email") String email, @FormParam("password") String password){
+        Response response;
+        try {
+            boolean authenticated = UsersController.checkUsersPassword(email, password);
+            response = Response.status(200).entity(authenticated).build();
+        } catch (NoSuchAlgorithmException|UnsupportedEncodingException |SQLException   e) {
             e.printStackTrace();
             response = Response.status(500).entity(e).build();
         }
